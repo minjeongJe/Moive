2ab7fd043adec06f06b9497047e339b6302a30af
minjeongJe <zzssjj@naver.com>
2024-09-01 04:00:29 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './NotFoundPage/NotFoundPage';
+import AppLayout from './layout/AppLayout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 홈페이지 /
+// 영화 전체보여주는 페이지 (서치) /movies
+// 영화 디테일 페이지 /movies/:id
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/movies">
+            <Route index element={<MoviePage />} />
+            <Route path=":id" element={<MovieDetailPage />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ src/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFoundPage.style.css'
+
+const NotFoundPage = () => {
+  return (
+      <div className={"text-center"} fluid>
+        <h1>404 not found</h1>
+      </div>
+  )
+}
+
+export default NotFoundPage
